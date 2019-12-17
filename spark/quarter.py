9e0704695ffecc62f40359c32224b49f338feeb5
limhyejin <hejin8307@naver.com>
2019-12-17 12:33:57 +0900

    final

--- spark/quarter.py
+++ spark/quarter.py
@@ -9,11 +9,11 @@ if __name__ == "__main__":
 
         df1 = spark.read.load("hdfs:///user/maria_dev/project", format="csv", sep=",", inferSchema="true", header="true", encoding="UTF-8")
 	df2 = df1.withColumn("year", split(col("date"), "\\.").getItem(0)).withColumn("month", split(col("date"), "\\.").getItem(1)).withColumn("day", split(col("date"), "\\.").getItem(2))
-        df2.createOrReplaceTempView("exchange")
+        df2.createOrReplaceTempView("quarter")
 
         result = spark.sql("""
                 SELECT country, quarter(from_unixtime(unix_timestamp(date, 'yyyy.MM.dd'), 'yyyy-MM-dd')) as quarter, avg(exchange) as average
-                FROM exchange
+                FROM quarter
 		WHERE year > '2004'
                 GROUP BY country, quarter
                 ORDER BY country, quarter
